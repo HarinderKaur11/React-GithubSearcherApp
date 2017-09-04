9d7714b61a6607ccdc18bbbe34f821232edab3c4
Harinder Kaur <snazyahuja11@gmail.com>
2017-09-04 17:53:49 -0400

    Pass component to Header for logout  operation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './Components/Header';
@@ -27,21 +27,85 @@ class App extends Component {
   componentWillMount(){
     this.lock = new Auth0Lock(this.props.clientID, this.props.domain);
     this.lock.on('authenticated', (authResult) => {
-      console.log(authResult);
+    //  console.log(authResult);
+    this.lock.getProfile(authResult.idToken, (error, profile) => {
+      if(error){
+        console.log(error);
+        return;
+      }
+    //  console.log( profile);\
+    this.setProfile(authResult.idToken, profile);
     });
+    });
+    this.getProfile(); // if the user is already signed in
+  }
+
+  setProfile(idToken, profile){
+    localStorage.setItem('idToken', idToken)
+    localStorage.setItem('profile', JSON.stringify(profile)) // since profile is a json object you can wrap it in javascript stringify ibject
+    //you can set as many objects here but they shld be in string format
+    this.setState({
+      idToken: localStorage.getItem('idToken'),
+      profile: JSON.parse(localStorage.getItem('profile'))
+    });
+  }
+
+  getProfile(){
+    // first check if the local storage is empty or has something
+    if(localStorage.getItem('idToken')!=null){
+      this.setState({
+        idToken: localStorage.getItem('idToken'),
+        profile: JSON.parse(localStorage.getItem('profile'))
+        //setStae can also take a callback function
+      }, () => {
+        console.log(this.state);
+      });
+    }
   }
 
   showLock(){
     this.lock.show();
   }
 
+  //logout code
+  //first set the local storage to remove the idToken and profile
+  //then set the state to be null
+  logout(){
+    this.setState({
+      idToken: '',
+      profile: ''
+      //use callback to clear up the local storage
+    }, () => {
+      localStorage.removeItem('idToken');
+      localStorage.removeItem('profile');
+    });
+    //since navigation bar is in a separate component we have to pass this logout there
+    //it can be passed as another prop
+  }
 
   render() {
+    let gitty;
+    // check whether user is logged in or not
+    //if the state variable of id token is empty it means that the user is not logged in
+    if(this.state.idToken){
+      gitty = <Github />
+    }else{
+      gitty = "Click on login to view Github Viewer"
+    }
+
     return (
       <div className="App">
+      {
+        //we also need to pass id and token as prop to check the login/logout state
+          }
         <Header
-        onLogin={this.showLock.bind(this)}/>
-        <Github />
+        lock={this.lock}
+        idToken={this.state.idToken}
+        profile={this.state.profile}
+        onLogout={this.logout.bind(this)}
+        onLogin={this.showLock.bind(this)}
+        />
+        {gitty}
       </div>
     );
   }
