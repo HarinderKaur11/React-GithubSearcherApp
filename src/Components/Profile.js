e42070f1dcf8ee7daf6f3a461788e28821850eee
Harinder Kaur <snazyahuja11@gmail.com>
2017-09-04 21:06:22 -0400

    Fetching Github API with fetch

---
+++ src/Components/Profile.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Profile extends Component {
+  render(){
+    return(
+      <div>
+        I am coming Profile
+      </div>
+    );
+  }
+}
+
+export default Profile;
--- src/Github.js
+++ src/Github.js
@@ -1,10 +1,61 @@
 import React, {Component} from 'react';
+import Profile from './Components/Profile';
+import Search from './Components/Search';
+
+const API = 'https://api.github.com/users'
 
 class Github extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      username: 'hiteshchoudhary',
+      name: '',
+      avatar: '',
+      repos:'',
+      followers:'',
+      following:'',
+      homeURL:'',
+      notFound:''
+    };
+  }
+
+  //method for fetching the api
+  getProfile(username){
+    //create a url
+    let finalURL = `${API}/${username}`;
+
+    //.then will take a callback ftn and give us res back
+    fetch(finalURL)
+    .then((res) => res.json())//res results a json object
+    .then((data) => {
+      this.setState({
+        username: data.login,
+        name: data.name,
+        avatar: data.avatar_url,
+        repos: data.public_repos,
+        followers: data.followers,
+        following: data.following,
+        homeURL: data.html_url,
+        notFound: data.message
+      });
+    })
+    .catch((error) => console.log('there was a problem in fetching data'))
+  }
+
+  componentDidMount(){
+    this.getProfile(this.state.username);
+
+  }
+
   render(){
     return(
       <div>
-        I am coming from Github
+        <section id="card">
+        <Search />
+        <Profile />
+        </section>
       </div>
     );
   }
